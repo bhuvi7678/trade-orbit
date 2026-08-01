284edbd5b93ee5ccbcea7830c96d86278d99bfc6
bhuvi7678 <bhupeshgupta673@gmail.com>
2026-08-01 23:45:32 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,44 +1,38 @@
-async function loadPrices() {
-    try {
-        const response = await fetch("https://api.coincap.io/v2/assets?ids=bitcoin,ethereum");
-        const result = await response.json();
-
-        document.getElementById("btc-price").innerText =
-            "$" + Number(result.data[0].priceUsd).toFixed(2);
-
-        document.getElementById("eth-price").innerText =
-            "$" + Number(result.data[1].priceUsd).toFixed(2);
-
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-async function searchCoin() {
-
-    let coin = document.getElementById("coin-input").value.toLowerCase().trim();
-
-    if (coin === "") {
-        alert("Please enter a coin name");
-        return;
-    }
+async function loadCrypto() {
 
     try {
 
-        const response = await fetch("https://api.coincap.io/v2/assets/" + coin);
-        const result = await response.json();
+        let response = await fetch(
+            "https://api.coincap.io/v2/assets?ids=bitcoin,ethereum"
+        );
 
-        document.getElementById("coin-name").innerText = result.data.name;
-        document.getElementById("coin-price").innerText =
-            "$" + Number(result.data.priceUsd).toFixed(2);
+        let data = await response.json();
 
-    } catch (error) {
+        let bitcoin = data.data[0];
+        let ethereum = data.data[1];
 
-        document.getElementById("coin-name").innerText = "Coin Not Found";
-        document.getElementById("coin-price").innerText = "Try: bitcoin, ethereum, solana";
+
+        document.getElementById("bitcoin").innerHTML =
+        "₿ Bitcoin: $" + Number(bitcoin.priceUsd).toFixed(2);
+
+
+        document.getElementById("ethereum").innerHTML =
+        "Ξ Ethereum: $" + Number(ethereum.priceUsd).toFixed(2);
+
+
+    } catch(error) {
+
+        document.getElementById("bitcoin").innerHTML =
+        "Bitcoin Error";
+
+        document.getElementById("ethereum").innerHTML =
+        "Ethereum Error";
 
     }
+
 }
 
-loadPrices();
-setInterval(loadPrices, 30000);
+
+loadCrypto();
+
+setInterval(loadCrypto, 30000);
