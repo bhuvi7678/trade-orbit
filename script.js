8beda3f1e808a88bd90bb1ac2f9af74fc4def8d5
bhuvi7678 <bhupeshgupta673@gmail.com>
2026-08-02 00:07:26 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,34 +4,25 @@ async function getCryptoData() {
 
     for (let coin of coins) {
 
-        const element = document.getElementById(coin);
-
-        if (!element) {
-            console.log("Missing ID:", coin);
-            continue;
-        }
-
         try {
 
-            const response = await fetch(
-                `https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=usd`
-            );
+            const url = `https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=usd`;
 
-            if (!response.ok) {
-                throw new Error("API Error");
-            }
+            const response = await fetch(url);
 
             const data = await response.json();
 
-            element.innerHTML = 
-            `${coin.toUpperCase()}: $${data[coin].usd}`;
+            console.log(data);
+
+            document.getElementById(coin).innerHTML =
+                `${coin.toUpperCase()}: $${data[coin].usd}`;
 
         } catch (error) {
 
-            element.innerHTML = 
-            `${coin.toUpperCase()} Data Not Available`;
+            console.log("Error:", error);
 
-            console.log(error);
+            document.getElementById(coin).innerHTML =
+                "API Error";
         }
     }
 }
