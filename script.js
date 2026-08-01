436368ef24fdeead0e5ab5af26a921fab946613c
bhuvi7678 <bhupeshgupta673@gmail.com>
2026-08-02 00:53:01 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
-async function getCryptoPrice() {
-  try {
-    const response = await fetch(
-      "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd"
-    );
-
-    const data = await response.json();
-
-    document.getElementById("bitcoin").innerHTML =
-      "$" + data.bitcoin.usd;
-
-    document.getElementById("ethereum").innerHTML =
-      "$" + data.ethereum.usd;
-
-  } catch (error) {
-    console.log(error);
-  }
+function fetchPrices() {
+  fetch("YOUR_API_URL")
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById("bitcoin").innerHTML =
+        "$" + data.bitcoin.usd;
+    })
+    .catch(error => console.log(error));
 }
 
-// first load
-getCryptoPrice();
+// Page load hote hi
+fetchPrices();
 
-// update every 10 seconds
-setInterval(getCryptoPrice, 10000);
+// Har 10 second me update
+setInterval(fetchPrices, 10000);
