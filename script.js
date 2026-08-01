a79e5e80358fdd0a397405ad34c45d468d015e90
bhuvi7678 <bhupeshgupta673@gmail.com>
2026-08-02 01:38:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,60 @@
-function fetchPrices() {
-  fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,binancecoin,solana&vs_currencies=usd")
-    .then(response => response.json())
-    .then(data => {
+let oldPrices = {};
 
-      document.getElementById("bitcoin").innerHTML =
-        "$" + data.bitcoin.usd;
+function updatePrice(id, newPrice) {
+  let element = document.getElementById(id);
 
-      document.getElementById("ethereum").innerHTML =
-        "$" + data.ethereum.usd;
+  if (oldPrices[id]) {
+    if (newPrice > oldPrices[id]) {
+      element.className = "price-up";
+      element.innerHTML = "$" + newPrice + " ↑";
+    } 
+    else if (newPrice < oldPrices[id]) {
+      element.className = "price-down";
+      element.innerHTML = "$" + newPrice + " ↓";
+    }
+    else {
+      element.innerHTML = "$" + newPrice;
+    }
+  } 
+  else {
+    element.innerHTML = "$" + newPrice;
+  }
 
-      document.getElementById("bnb").innerHTML =
-        "$" + data.binancecoin.usd;
-
-      document.getElementById("solana").innerHTML =
-        "$" + data.solana.usd;
-
-    })
-    .catch(error => {
-      console.log("Price Error:", error);
-    });
+  oldPrices[id] = newPrice;
 }
+
+
+function fetchPrices() {
+
+  fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,binancecoin,solana&vs_currencies=usd")
+
+  .then(response => response.json())
+
+  .then(data => {
+
+    updatePrice("bitcoin", data.bitcoin.usd);
+    updatePrice("ethereum", data.ethereum.usd);
+    updatePrice("bnb", data.binancecoin.usd);
+    updatePrice("solana", data.solana.usd);
+
+
+    document.getElementById("update-time").innerHTML =
+    "Last Update: " + new Date().toLocaleTimeString();
+
+  })
+
+  .catch(error => {
+    console.log("Price Error:", error);
+  });
+
+}
+
 
 // First load
 fetchPrices();
 
+
 // Auto update every 10 seconds
 setInterval(fetchPrices, 10000);
+
 console.log("Script loaded");
-setInterval(fetchPrices, 10000);
