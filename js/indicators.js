ad82532f70075a1b9972eb710e3e1e029e5ba969
bhuvi7678 <bhupeshgupta673@gmail.com>
2026-08-02 17:21:41 +0530

    Add EMA calculation and charting functions
    Implemented functions to calculate and add EMA lines to the chart.

--- js/indicators.js
+++ js/indicators.js
@@ -1,1 +1,97 @@
+// =========================
+// Trade Orbit V6 - indicators.js
+// =========================
 
+
+// Calculate EMA
+
+function calculateEMA(data, period){
+
+    let ema = [];
+
+    let multiplier = 2 / (period + 1);
+
+
+    let previousEMA = data[0].close;
+
+
+    data.forEach((candle, index)=>{
+
+
+        if(index === 0){
+
+            ema.push({
+                time:candle.time,
+                value:previousEMA
+            });
+
+        }
+        else{
+
+            let currentEMA =
+            (candle.close - previousEMA) * multiplier
+            + previousEMA;
+
+
+            previousEMA = currentEMA;
+
+
+            ema.push({
+
+                time:candle.time,
+
+                value:currentEMA
+
+            });
+
+        }
+
+
+    });
+
+
+    return ema;
+
+}
+
+
+// Add EMA Lines
+
+function addEMA(chart, candleData){
+
+
+    const ema9 =
+    chart.addLineSeries({
+        lineWidth:2
+    });
+
+
+    const ema20 =
+    chart.addLineSeries({
+        lineWidth:2
+    });
+
+
+    const ema200 =
+    chart.addLineSeries({
+        lineWidth:2
+    });
+
+
+
+    ema9.setData(
+        calculateEMA(candleData,9)
+    );
+
+
+    ema20.setData(
+        calculateEMA(candleData,20)
+    );
+
+
+    ema200.setData(
+        calculateEMA(candleData,200)
+    );
+
+
+}
