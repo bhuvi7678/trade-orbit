574f4aaa62636d9de482c8d36d8bf0d6c41d9bb0
bhuvi7678 <bhupeshgupta673@gmail.com>
2026-08-02 13:06:07 +0530

    Add event listener for DOMContentLoaded

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
+// =========================
+// Trade Orbit V4 - app.js
+// =========================
 
+
+// Website Load
+
+document.addEventListener("DOMContentLoaded", function(){
+
+
+    // First API Load
+
+    if(typeof fetchCryptoData === "function"){
+
+        fetchCryptoData();
+
+    }
+
+
+    // Search Start
+
+    if(typeof setupSearch === "function"){
+
+        setupSearch();
+
+    }
+
+
+});
